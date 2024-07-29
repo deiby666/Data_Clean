61442f2e7aa93364050f375698a11f8b6e40807e
jhonfabernorena <jhonfabernorenapineres@gmail.com>
2024-07-29 12:25:34 -0400

    entity for saving the apikey

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FilesModule } from './files/files.module';
+import { ApikeyModule } from './apikey/apikey.module';
 
 
 @Module({
-  imports: [FilesModule],
+  imports: [FilesModule, ApikeyModule],
   controllers: [],
   providers: [],
 })
