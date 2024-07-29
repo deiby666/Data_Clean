32a4ce9da56490ba4c9f764cd7b4cfa15a9faa2a
cristianManco <camilomanco2005@gmail.com>
2024-07-29 11:40:49 -0500

    feat: create logs module and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FilesModule } from './files/files.module';
-
+import { LogModule } from './log/log.module';
 
 @Module({
-  imports: [FilesModule],
+  imports: [FilesModule, LogModule],
   controllers: [],
   providers: [],
 })
